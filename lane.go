df3c304d3b0acaa8b95048ecc934766c1e1a4ab7
ItsJimi <jimi.maiquez@gmail.com>
2018-06-17 23:28:44 +0200

    create Lane struct to store routes

--- lane.go
+++ lane.go
@@ -5,33 +5,16 @@ import (
 	"net/http"
 )
 
-// Context context
-type Context struct {
-	Response http.ResponseWriter
-	Request  *http.Request
+// Lane object
+type Lane struct {
+	Routes []string
 }
 
-// Send send simple response
-func (c *Context) Send(str string) {
-	c.Response.Write([]byte(str))
-}
-
-// Get handle get method
-func Get(pattern string, handler func(Context)) {
-	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			return
-		}
-
-		handler(Context{
-			Response: w,
-			Request:  r,
-		})
-	})
-}
+var mux = http.NewServeMux()
 
 // Start start webserver
 func Start(addr string) error {
 	fmt.Println("Lane started")
-	return http.ListenAndServe(addr, nil)
+
+	return http.ListenAndServe(addr, mux)
 }
